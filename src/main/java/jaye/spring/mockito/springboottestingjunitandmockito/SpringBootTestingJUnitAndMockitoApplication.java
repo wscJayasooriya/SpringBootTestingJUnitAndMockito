34ba3e12ff7adf28843236e1312a67af3e86ab61
Chinthaka Jayasooriya <jayasooriya.sc@gmail.com>
2024-06-25 21:07:44 +0530

    create project and setup

---
+++ src/main/java/jaye/spring/mockito/springboottestingjunitandmockito/SpringBootTestingJUnitAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package jaye.spring.mockito.springboottestingjunitandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingJUnitAndMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestingJUnitAndMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jaye/spring/mockito/springboottestingjunitandmockito/SpringBootTestingJUnitAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package jaye.spring.mockito.springboottestingjunitandmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingJUnitAndMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
